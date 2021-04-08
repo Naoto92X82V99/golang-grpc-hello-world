1ce299aaa31099f1efd1f2618565458685c70b38
kaneko92X82V99 <naoto5018@gmail.com>
2021-04-08 18:26:57 +0900

    feat: GoでGRPCを用いる際の最小構成を作成

---
+++ go.mod
@@ -0,0 +1,10 @@
+module js-pro-ingress-hosts
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.0
+	google.golang.org/grpc v1.37.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/protobuf v1.26.0
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	pb "js-pro-ingress-hosts/helloworld"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50052"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	// fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
